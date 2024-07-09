ece2e7aded6319621e61dfc3b611eec4f0a528ab
tanishqmalik <tanishqvideo@gmail.com>
2024-07-09 18:07:13 +0530

    ok

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -8,8 +8,9 @@ class ApiError extends Error{
         super(message)
         this.statusCode= statusCode
         this.data= null
-        this.message=false;
-        this.errors= this.errors
+        this.message=message
+        this.success= false;
+        this.errors= errors
 
         if(stack){
             this.stack= stack
